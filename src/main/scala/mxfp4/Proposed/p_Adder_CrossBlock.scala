eefddc3aa1d9cdd7af5cae69165d9eb78b73dec6
Wonjun-Han <wonjun.han@snu.ac.kr>
2025-08-06 12:58:16 +0900

    test

--- src/main/scala/mxfp4/Proposed/p_Adder_CrossBlock.scala
+++ src/main/scala/mxfp4/Proposed/p_Adder_CrossBlock.scala
@@ -5,11 +5,15 @@ import chisel3.util._
 import _root_.circt.stage.ChiselStage
 
 class p_Adder_CrossBlock_IO (d: Int) extends Bundle {
-    val in = Input(Vec(8, SInt((10+d).W)))
+    val in = Input(Vec(8, SInt((8+d).W)))
     val exp = Input(Vec(8, SInt(10.W)))
     val depth = Input(UInt(3.W))
     val nan = Input(Vec(8, UInt(1.W)))
     val out = Output(Vec(8, new FP32))
 }
 
-class 
+class p_Adder_CrossBlock extends Module {
+    val io = IO(new p_Adder_CrossBlock_IO(6))
+    
+
+}
